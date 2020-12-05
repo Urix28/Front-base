3cc01dd8ba07223067a9146435d66e1d0302ce2e
Urix28 <urielriveros62@gmail.com>
2020-12-04 18:57:59 -0600

    v1

---
+++ main.js
@@ -0,0 +1,76 @@
+let nCount = selector => {
+  $(selector).each(function () {
+    $(this)
+      .animate({
+        Counter: $(this).text()
+      }, {
+        // A string or number determining how long the animation will run.
+        duration: 4000,
+        // A string indicating which easing function to use for the transition.
+        easing: "swing",
+        /**
+         * A function to be called for each animated property of each animated element. 
+         * This function provides an opportunity to
+         *  modify the Tween object to change the value of the property before it is set.
+         */
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+  });
+};
+
+let a = 0;
+$(window).scroll(function () {
+  // The .offset() method allows us to retrieve the current position of an element  relative to the document
+  let oTop = $(".numbers").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    nCount(".rect > h1");
+  }
+});
+
+
+
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
+
+var ctxL = document.getElementById("lineChart").getContext('2d');
+
+var myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [0, 65, 45, 65, 35, 65, 0],
+        backgroundColor: '#AD35BA',
+        borderColor: [
+          '#AD35BA',
+        ],
+        borderWidth: 2,
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(173, 53, 186, 0.1)",
+      }
+    ]
+  },
+  options: {
+    responsive: true
+  }
+});
